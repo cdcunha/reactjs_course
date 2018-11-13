0e274e96e1db87d26576e5500be35daa78002e1a
cdcunha <cdcunha@gmail.com>
2018-11-13 17:32:42 -0200

    Commit referente à aula 43 - Sessão 4

--- session4/react-blog/src/index.js
+++ session4/react-blog/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => {
+    return <h1>This is the Home page</h1>
+}
+
+const About = () => {
+    return <h1>This is the About page</h1>
+}
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/about" component={About}/>
+            <Route path="/Home" component={Home}/>
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

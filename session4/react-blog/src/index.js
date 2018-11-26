7d2da39326b04092f72e764f66d865c713c9251e
cdcunha <cdcunha@gmail.com>
2018-11-26 17:51:22 -0200

    Conclusão do da Sessão 4 - Routing react applications with React Router 4

--- session4/react-blog/src/index.js
+++ session4/react-blog/src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import './index.css';
-import App from './App';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
+import SingleArticle from './components/SingleArticle';
+import CreateArticle from './components/CreateArticle';
+
 import * as serviceWorker from './serviceWorker';
-
-const Home = () => {
-    return <h1>This is the Home page</h1>
-}
-
-const About = () => {
-    return <h1>This is the About page</h1>
-}
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Route exact path="/" component={App}/>
-            <Route path="/about" component={About}/>
-            <Route path="/Home" component={Home}/>
+            <Navbar/>
+            <Route exact path="/" component={Welcome}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/article/:slug" component={SingleArticle}/>
+            <Route path="/articles/create" component={CreateArticle}/>
+            <Footer/>
         </div>
     </BrowserRouter>
     , document.getElementById('root')

2b6abb3c6c01d25c4cf6adaf47cb2f6582f03e7c
cdcunha <cdcunha@gmail.com>
2018-11-30 14:30:40 -0200

    Session 5, class 56

--- session5/react-concepts/src/App.js
+++ session5/react-concepts/src/App.js
@@ -3,9 +3,19 @@ import Alert from './ui-components/Alert';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showAlert: true
+    }
+  }
+
   render() {
     return (
       <div className="container">
+        <Alert type="warning" show={this.state.showAlert}>
+          <small>Small message</small>
+        </Alert>
         <Alert type="success">
           <h2 className="alert-heading">This is an alert heading</h2>
         </Alert>
